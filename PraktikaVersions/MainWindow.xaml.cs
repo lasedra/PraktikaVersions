6c11b649af60fe93e8de74d870ad68fc9f54086e
lasedra <iliakozitskii04@gmail.com>
2024-01-13 16:23:49 +0300

    Connection exceptions handled

--- PraktikaVersions/MainWindow.xaml.cs
+++ PraktikaVersions/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using PraktikaVersions.Models;
 using Squirrel;
+using System;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace PraktikaVersions
@@ -18,16 +20,35 @@ namespace PraktikaVersions
 
         private async void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            updateManager = await UpdateManager.GitHubUpdateManager(@"https://github.com/lasedra/PraktikaVersions");
-            VersionTextBox.Text += updateManager.CurrentlyInstalledVersion().ToString();
-
-            var updateInfo = await updateManager.CheckForUpdate();
-            if (updateInfo.ReleasesToApply.Count > 0)
+            try
             {
-                if (MessageBox.Show("Update the app?", "New version is available!", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                updateManager = await UpdateManager.GitHubUpdateManager(@"https://github.com/lasedra/PraktikaVersions");
+                if (updateManager.CurrentlyInstalledVersion() != null)
+                    VersionTextBox.Text = updateManager.CurrentlyInstalledVersion().ToString();
+                else
+                    VersionTextBox.Text = "...";
+
+                var updateInfo = await updateManager.CheckForUpdate();
+                if (updateInfo.ReleasesToApply.Count > 0)
                 {
-                    await updateManager.UpdateApp();
-                    MessageBox.Show("Please, restart the application to apply a new version", "Updated successfully!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    if (MessageBox.Show("Update the app?", "New version is available!", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                    {
+                        await updateManager.UpdateApp();
+                        MessageBox.Show("Please, restart the application to apply a new version", "Updated successfully!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex is System.Net.Http.HttpRequestException)
+                {
+                    VersionStackPanel.Visibility = Visibility.Hidden;
+                    FailTextBox.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    // Обработка других типов исключений
+                    MessageBox.Show("Unknown error: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
             }
         }
