54a7b76005711a53994bd86510a28ad33f0a5447
lasedra <iliakozitskii04@gmail.com>
2024-01-18 10:14:45 +0300

    Temp

---
+++ PraktikaVersions/DownloadWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Octokit;
+using System;
+using System.Net;
+using System.Windows;
+using System.IO;
+
+namespace PraktikaVersions
+{
+    public partial class DownloadWindow : Window
+    {
+        #region Disallow_closing
+        // Prep stuff needed to remove close button on window
+        private const int GWL_STYLE = -16;
+        private const int WS_SYSMENU = 0x80000;
+        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        void ToolWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Code to remove close box from window
+            var hwnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
+            SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_SYSMENU);
+        }
+        #endregion
+
+        GitHubClient gitClient = null!;
+        Release releaseToDownload = null!;
+        string updatesFolderFullPath = Path.Combine(GetParentDirectory(AppDomain.CurrentDomain.BaseDirectory, 4), @"updates");
+
+        public DownloadWindow(GitHubClient _gitClient, Release _releaseToDownload)
+        {
+            InitializeComponent();
+            Loaded += ToolWindow_Loaded;
+
+            this.gitClient = _gitClient;
+            this.releaseToDownload = _releaseToDownload;
+            TagNameTextBox.Text += releaseToDownload.TagName;
+            NoteTextBox.Text = releaseToDownload.Body;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            using(WebClient client = new WebClient())
+            {
+                client.Headers.Add("Authorization", $"Bearer {gitClient.Credentials.GetToken()}");
+                client.Headers.Add("User-Agent", "smth");
+
+                client.DownloadProgressChanged += (s, e)
+                    => ProgressBar.Value = e.ProgressPercentage;
+                client.DownloadFileCompleted += (s, e) =>
+                { 
+                    MessageBox.Show("The download is done!"); 
+                    this.Close(); 
+                };
+
+                // TODO: Нужно сделать либо разархивацию, либо скачивать только .exe, через releaseToDownload.Assets[n] (а архив отдельно)
+                client.DownloadFileAsync(new Uri(releaseToDownload.Url), $@"{updatesFolderFullPath}\PraktikaVersions-{releaseToDownload.TagName} version.zip");
+            }
+        }
+
+        private static string GetParentDirectory(string path, int levels)
+        {
+            for (int i = 0; i < levels; i++)
+                path = Directory.GetParent(path).FullName;
+            return path;
+        }
+    }
+}
--- PraktikaVersions/MainWindow.xaml.cs
+++ PraktikaVersions/MainWindow.xaml.cs
@@ -2,49 +2,64 @@
 using PraktikaVersions.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
+using System.Reflection;
 using System.Windows;
 
 namespace PraktikaVersions
 {
     public partial class MainWindow : Window
     {
+        public class Internet
+        {
+            public static bool IsOk()
+            {
+                try
+                {
+                    Dns.GetHostEntry("github.com");
+                    return true;
+                }
+                catch { return false; }
+            }
+        }
         GitHubClient gitClient = null!;
         IReadOnlyList<Release> releases = null!;
+        string exeName = AppDomain.CurrentDomain.FriendlyName + ".exe";
+        string exePath = Process.GetCurrentProcess().MainModule.FileName;
 
         public MainWindow()
         {
             InitializeComponent();
             UserListView.ItemsSource = PracticeDbContext.dbContext.Users.ToList();
-            Loaded += Window_Loaded;
+            Directory.SetCurrentDirectory(AppContext.BaseDirectory);
         }
+
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            VersionTextBox.Text += Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
             UpdateChecker();
             GetReleases();
         }
 
 
-        private async void UpdateBttn_Click(object sender, RoutedEventArgs e)
+        private void UpdateBttn_Click(object sender, RoutedEventArgs e)
         {
-            if(VersionsComboBox.SelectedItem != null)
+            if (VersionsComboBox.SelectedItem != null)
             {
-                var releaseToDownload = releases.FirstOrDefault(r => r.TagName == ((Release)VersionsComboBox.SelectedItem).TagName);
-
-                var result = MessageBox.Show($"Are you surer you wanna update to version: {releaseToDownload.TagName}?", "Warning!", MessageBoxButton.YesNo, MessageBoxImage.Warning);
-                if (result == MessageBoxResult.Yes)
+                var releaseToDownload = releases.First(r => r.TagName == ((Release)VersionsComboBox.SelectedItem).TagName);
+                if (releaseToDownload.TagName != VersionTextBox.Text) //TODO: Проверка на версию
                 {
-                    using (WebClient client = new())
+                    var result = MessageBox.Show($"Are you sure you wanna update to version: {releaseToDownload.TagName}?", "Warning!", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                    if (result == MessageBoxResult.Yes)
                     {
-                        client.Headers.Add("Authorization", $"Bearer {gitClient.Credentials.GetToken()}");
-                        client.Headers.Add("User-Agent", "smth");
-
-                        await client.DownloadFileTaskAsync(releaseToDownload.ZipballUrl,
-                            $@"{Path.Combine(GetParentDirectory(AppDomain.CurrentDomain.BaseDirectory, 4), @"updates")}\{releaseToDownload.TagName} version.zip");
-                        //TODO: Скачивается не полностью и долго. Нет прогресс бара.
+                        DownloadWindow downloadWindow = new DownloadWindow(gitClient, releaseToDownload);
+                        downloadWindow.ShowDialog();
+                        // TODO: Нужно сделать либо разархивацию, либо скачивать только .exe, через releaseToDownload.Assets[n] (а архив отдельно)
+                        //Cmd($"taskkill /f /im \"{exeName}\" && timeout /t 1 && del \"{exePath}\" && ren \"{downloadedFile}\" \"{exePath}\" && \"{exePath}\"");
                     }
                 }
             }
@@ -52,31 +67,25 @@ namespace PraktikaVersions
 
         private async void GetReleases()
         {
-            try
+            if (Internet.IsOk())
             {
                 gitClient = new GitHubClient(new ProductHeaderValue("praktika-versions"))
-                { Credentials = new Credentials("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") };
+                { Credentials = new Credentials("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") }; //TODO: Разобраться с токеном
                 string userName = "lasedra",
                        reposName = "PraktikaVersions";
 
                 releases = await gitClient.Repository.Release.GetAll(userName, reposName);
                 if (releases != null)
                     VersionsComboBox.ItemsSource = releases;
-                else 
+                else
                 {
                     //TODO: Отображать "Отсутствие каких-либо версий"
                 }
-
             }
-            catch (Exception ex)
+            else
             {
-                if (ex is System.Net.Http.HttpRequestException)
-                {
-                    FailTextBox.Visibility = Visibility.Visible;
-                    VersionsPanel.Visibility = Visibility.Hidden;
-                }
-                else
-                    MessageBox.Show("Unknown error: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                FailTextBox.Visibility = Visibility.Visible;
+                VersionsPanel.Visibility = Visibility.Hidden;
             }
         }
 
@@ -85,13 +94,20 @@ namespace PraktikaVersions
             //TODO: Проверятель обновлений
         }
 
-        private static string GetParentDirectory(string path, int levels)
+        private void Cmd(string line)
         {
-            for (int i = 0; i < levels; i++)
+            try
             {
-                path = Directory.GetParent(path).FullName;
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = $"/c {line}",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true,
+                });
             }
-            return path;
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
         }
     }
 }
